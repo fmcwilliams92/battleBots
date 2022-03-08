d9b70eead495e446414bc593a5dc0eb4bf0e5049
Fred McWilliams <fredmcwilliams92@gmail.com>
2022-03-07 21:18:34 -0600

    pushing to switch branches

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,14 @@
 var playerName = window.prompt("What is the name of your robot?");
 console.log(playerName);
 
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+
+console.log(10 + 10);
+// what is this
+
+console.log("Our robots name is " + playerName);
+
 // this creates a function named "fight"
 function fight() {
   window.alert("The fight has begun!");
