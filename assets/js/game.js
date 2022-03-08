9ec99421d7d249e81ef08b072b1915285098d592
Fred McWilliams <fredmcwilliams92@gmail.com>
2022-03-07 18:42:59 -0600

    adding additional branches

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JS is running!")
+// this prompt asks for your robots name
+var playerName = window.prompt("What is the name of your robot?");
+console.log(playerName);
+
+// this creates a function named "fight"
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+fight();
